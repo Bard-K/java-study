44fbc382df5b59de8880010d05928e7ead22a5b5
Bard-K <tngksmf103@gmail.com>
2024-03-15 23:56:29 +0900

    test: lambda 테스트

---
+++ src/main/java/bard/javastudy/api/controller/StudyController.java
@@ -0,0 +1,12 @@
+package bard.javastudy.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/study")
+public class StudyController {
+
+}
--- src/test/java/bard/javastudy/JavaStudyApplicationTests.java
+++ src/test/java/bard/javastudy/JavaStudyApplicationTests.java
@@ -3,11 +3,25 @@ package bard.javastudy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.function.Function;
+
 @SpringBootTest
 class JavaStudyApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void lambdaMethodReference() {
+
+        Function<Integer, int[]> f = x -> new int[x]; // 람다식
+        Function<Integer, int[]> f2 = int[]::new; // 메서드 참조
+    }
+
+    @Test
+    void stream() {
+
     }
 
 }
